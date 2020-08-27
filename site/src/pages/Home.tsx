ab0ff6361d8a0cbc9034c0592f8f48955a1c1e6b
tchardin <tdotchardin@gmail.com>
2020-08-27 10:15:15 +0200

    Email form

--- site/src/pages/Home.tsx
+++ site/src/pages/Home.tsx
@@ -1,19 +1,33 @@
-import * as React from "react";
+import React, { Suspense } from "react";
 import styles from "./Home.module.css";
+
+import EmailInput from "../components/EmailInput";
 
 const NNCancas = React.lazy(() => import("../effects/NNCanvas"));
 
 const Hero = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.titleContainer}>
-        <NNCancas />
-        <h1 className={styles.title}>Myel</h1>
-        <p className={styles.subtitle}>
-          Earn income with your computer's extra storage
-        </p>
-      </div>
-    </main>
+    <>
+      <main className={styles.main}>
+        <div className={styles.titleContainer}>
+          <Suspense fallback={null}>
+            <NNCancas />
+          </Suspense>
+          <h1 className={styles.title}>Myel</h1>
+          <p className={styles.subtitle}>
+            Earn income with your computer's extra storage
+          </p>
+        </div>
+        <EmailInput />
+      </main>
+      <footer className={styles.footer}>
+        <nav className={styles.footerNav}>
+          <a className={styles.link} href="https://github.com/tchardin/myel">
+            Github
+          </a>
+        </nav>
+      </footer>
+    </>
   );
 };
 
