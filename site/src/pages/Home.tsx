414af51632ce89cfd78a44d7d6f425abbef36915
tchardin <tdotchardin@gmail.com>
2020-08-30 22:18:25 +0200

    Illustration and app screenshot

--- site/src/pages/Home.tsx
+++ site/src/pages/Home.tsx
@@ -1,6 +1,9 @@
 import React, { Suspense } from "react";
 import styles from "./Home.module.css";
-import { ReactComponent as Blob } from "../effects/Blob.svg";
+import Dashboard from "../assets/LatestDashboard.png";
+import Dashboard2 from "../assets/LatestDashboard@2x.png";
+import Dashboard3 from "../assets/LatestDashboard@3x.png";
+import Illustration from "../assets/Illustration-03.png";
 
 const EmailInput = React.lazy(() => import("../components/EmailInput"));
 const NNCancas = React.lazy(() => import("../effects/NNCanvas"));
@@ -17,11 +20,43 @@ const Hero = () => {
           <p className={styles.subtitle}>
             Earn income with your computer's extra storage
           </p>
+          <section className={styles.section}>
+            <p className={styles.info}>
+              For anyone with lots of unused hard drive space, Myel makes
+              getting up and running with{" "}
+              <a
+                className={styles.inlineLink}
+                href="https://filecoin.io"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Filecoin
+              </a>{" "}
+              as simple as installing any desktop app.
+            </p>
+            <figure className={styles.sideImage}>
+              <img
+                src={Illustration}
+                alt="Individual with a personal computer"
+                className={styles.illustration}
+              />
+            </figure>
+          </section>
         </div>
+        <figure className={styles.imgFigure}>
+          <img
+            src={Dashboard}
+            srcSet={`${Dashboard} 1x, ${Dashboard2} 2x, ${Dashboard3} 3x, ${Dashboard}`}
+            alt="Dashboard preview"
+            className={styles.dashboardImg}
+          />
+        </figure>
+      </main>
+      <div className={styles.formSection}>
         <Suspense fallback={null}>
           <EmailInput />
         </Suspense>
-      </main>
+      </div>
       <footer className={styles.footer}>
         <nav className={styles.footerNav}>
           <a className={styles.link} href="https://github.com/tchardin/myel">
@@ -29,9 +64,6 @@ const Hero = () => {
           </a>
         </nav>
       </footer>
-      <div className={styles.blobContainer}>
-        <Blob width="100%" />
-      </div>
     </>
   );
 };
